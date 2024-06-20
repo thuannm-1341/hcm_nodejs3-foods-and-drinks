0d21439109da09e54775d2deb36e0d2345ee4a9e
thuannm-1341 <nguyen.minh.thuan-b@sun-asterisk.com>
2024-06-20 15:50:59 +0700

    admin view user list and user detail

--- src/routes/admin.route.ts
+++ src/routes/admin.route.ts
@@ -3,14 +3,21 @@ import { authenticateAdminRole } from '../middlewares/auth.middleware';
 import { AdminController } from '../controllers/admin/admin.controller';
 import { Router } from 'express';
 import { AdminProductController } from '../controllers/admin/product.controller';
+import { AdminUserController } from '../controllers/admin/user.controller';
 const adminRoute: Router = Router();
 const adminController = new AdminController();
 const adminOrderController = new AdminOrderController();
 const adminProductController = new AdminProductController();
+const adminUserController = new AdminUserController();
 
 adminRoute.use(authenticateAdminRole);
 adminRoute.get('/home', adminController.getAdminDashboard);
+
 adminRoute.get('/order', adminOrderController.getAdminOrderPage);
+adminRoute.put('/order/update-store', adminOrderController.updateOrderStore);
+adminRoute.put('/order/update', adminOrderController.updateOrderStatus);
+adminRoute.get('/order/:id', adminOrderController.getOrderDetail);
+
 adminRoute.get('/product/search', adminProductController.getAdminProductPage);
 adminRoute.get('/product/create', adminProductController.adminCreateProductGet);
 adminRoute.post(
@@ -26,8 +33,8 @@ adminRoute.post(
   adminProductController.adminUpdateProductPost,
 );
 adminRoute.get('/product/:id', adminProductController.getAdminProductDetail);
-adminRoute.put('/order/update-store', adminOrderController.updateOrderStore);
-adminRoute.put('/order/update', adminOrderController.updateOrderStatus);
-adminRoute.get('/order/:id', adminOrderController.getOrderDetail);
+
+adminRoute.get('/user', adminUserController.getAdminUserPage);
+adminRoute.get('/user/:id', adminUserController.adminGetUserDetail);
 
 export default adminRoute;
